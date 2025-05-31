9685481210985ecfdb46d7f88e2b8717231cb1ea
snehilshah <snehilshah27@gmail.com>
2025-05-31 23:48:04 +0530

    :exclamation: backup for arch installation btw

--- PDF/ReadData.py
+++ PDF/ReadData.py
@@ -8,9 +8,9 @@ for file in files:
     pdf = pdfquery.PDFQuery(file)
     pdf.load(0)
 
-
-#convert the pdf to XML
-    id = pdf.pq('LTTextLineHorizontal:in_bbox("137.93, 137.518, 186.399, 147.478")').text()
-    res = id.strip().strip('-').strip()
+    # convert the pdf to XML
+    id = pdf.pq(
+        'LTTextLineHorizontal:in_bbox("137.93, 137.518, 186.399, 147.478")'
+    ).text()
+    res = id.strip().strip("-").strip()
     print(res)
-
